2506747d5d70821b214881d157e4c264b1c1dd0e
scott.hsieh[謝書正] <scott.hsieh@scotthsieh-mac13.local>
2021-06-28 00:25:42 +0800

    make project settings

--- .projenrc.js
+++ .projenrc.js
@@ -1,19 +1,63 @@
-const { AwsCdkConstructLibrary } = require('projen');
+const { AwsCdkConstructLibrary, NpmAccess, ProjectType } = require('projen');
 const project = new AwsCdkConstructLibrary({
-  author: 'user',
-  authorAddress: 'user@domain.com',
-  cdkVersion: '1.95.2',
+  author: 'scott.hsieh',
+  authorName: 'Shu-Jeng Hsieh',
+  authorAddress: 'https://fantasticsie.medium.com/',
+  keywords: [
+    'amazon-comprehhend',
+    'aws-sam',
+    'object-lambda',
+    'pii',
+    's3',
+    'scott.hsieh'
+  ],
+
+  catalog: {
+    announce: true
+  },
+
+  cdkVersion: '1.110.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-comprehend-s3olap',
   repositoryUrl: 'git@scott:HsiehShuJeng/cdk-comprehend-s3olap.git',
+  projectType: ProjectType.LIB,
 
-  // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
-  // cdkTestDependencies: undefined,    /* AWS CDK modules required for testing. */
-  // deps: [],                          /* Runtime dependencies of this module. */
-  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],                       /* Build dependencies for this module. */
-  // packageName: undefined,            /* The "name" in package.json. */
-  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
-  // releaseWorkflow: undefined,        /* Define a GitHub workflow for releasing from "main" when new versions are bumped. */
+  cdkDependencies: [
+    '@aws-cdk/aws-s3',
+  ],
+  cdkAssert: true,
+  npmAccess: NpmAccess.PUBLIC,
+
+  eslint: true,
+  projenUpgradeSecret: 'PROJEN_UPGRADE_SECRET',
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+  },
+  depsUpgradeAutoMerge: true,
+
+  // publish to npm
+  releaseToNpm: true,
+  releaseWorkflow: true,
+  releaseEveryCommit: true, //will run the release GitHub Action on each push to the defined
+
+  // publish to PyPi
+  publishToPypi: {
+    distName: 'cdk_comprehend_s3olap',
+    module: 'cdk_comprehend_s3olap',
+  },
+
+  // publish to Maven
+  publishToMaven: {
+    mavenGroupId: 'io.github.hsiehshujeng',
+    mavenArtifactId: 'cdk-comprehend-s3olap',
+    javaPackage: 'io.github.hsiehshujeng.cdk.comprehend.s3olap',
+    mavenEndpoint: 'https://s01.oss.sonatype.org', // check https://central.sonatype.org/publish/release/#login-into-ossrh
+  },
+
+  // publish to dotnet
+  publishToNuget: {
+    dotNetNamespace: 'ScottHsieh.Cdk',
+    packageId: 'Comprehend.S3olap',
+  },
 });
 project.synth();
